abb3e40003e7cbad6102844143acc9009c213dd7
EstroniloShirley <imshaneestronilo@gmail.com>
2023-11-10 15:21:38 +0800

    02createdHtmlforHOME

--- home.php
+++ home.php
@@ -1,11 +1,54 @@
+<?php
+// echo 'Hello Shane Estronilo';
+// locale_filter_matchesgg
+
+// print 'Hello from print'
+// print_r ([1,2,3]);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>BHCMS</title>
 </head>
 <body>
-    
+    <header><h1>Barangay Ngalan</h1>
+        <nav><ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Appointment</a></li>
+            <li><a href="#">Sign in</a></li>
+    </ul></nav>
+    </header>
+    <Nav className= 'sidenav'>
+       <ul>
+        <li><a href="#">LOGO image</a></li>
+        <li><a href="#">DASHBOARD</a></li>
+        <li><a href="#">APPOINTMENTS</a></li>
+        <li><a href="#">PATIENTS</a></li>
+        <li><a href="#">USERS</a></li>
+       </ul>
+    <div>Signed in Profile</div>
+    <a href="#">Sign out</a>
+    </Nav>
+    <footer>
+        <a href="#">LOGO</a>
+        <a href="#"><i>fb icon</i></a>
+        <a href="#"><i>tweeter icon</i></a>
+        <a href="#"><i>instagram icon</i></a>
+        <form action="#">Subscribe</form>
+        <a href="#"><i>Privacy Policy</i></a>
+        <a href="#"><i>Terms and Condition</i></a>
+        <div><span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2023
+         <a href="#">Barangay Ngalan</a>
+          . All Rights Reserved.
+        </span></div>
+</footer>
+
 </body>
 </html>
